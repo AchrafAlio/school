62ac2982b51c9cfb5f65003eff6ddcd5aca6ae6b
Achraf Alioui <aa@softifi.com>
2022-10-24 08:27:53 +0100

    modif student_user_view.xml and student.py

--- custom/school_management/models/student_user.py
+++ custom/school_management/models/student_user.py
@@ -2,10 +2,10 @@ from odoo import api, fields, models
 
 
 class SchoolStudentUser(models.Model):
-    _name = "school.student.user"
+    # _name = "school.student"
     _inherit = 'school.student'
     _description = "School Student User"
 
-    @api.model
-    def create(self, vals):
-        return super('school.student', self).create(vals)
+    # @api.model
+    # def create(self, vals):
+    #     return super('school.student', self).create(vals)
